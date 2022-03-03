d50cf9227cae1a472044b177e7febd6961702d7b
rownox <rownox.ik@gmail.com>
2022-03-03 15:50:22 -0500

    info cmds 3

--- src/main/java/net/tazpvp/tazpvpcore/commands/infoCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/infoCommand.java
@@ -20,7 +20,7 @@ public class infoCommand {
                         ChatColor.GOLD + "mining, and daily rewards to use at the shop\n" +
                         ChatColor.YELLOW + "\n" +
                         ChatColor.RED + "Get up to five extra hearts from streaks,\n" +
-                        ChatColor.RED + "rebirthing, or getting the \"fat\" perk.\n" +
+                        ChatColor.RED + "rebirth, or getting the \"fat\" perk.\n" +
                         ChatColor.YELLOW + "\n" +
                         ChatColor.DARK_AQUA + "All inventory items are lost on death.\n" +
                         ChatColor.GREEN + "PAGE [1/2]" + ChatColor.GRAY + "/help2 for next page."
@@ -37,7 +37,22 @@ public class infoCommand {
                         ChatColor.GREEN + "PAGE [2/2] " + ChatColor.GRAY + "/help to go back"
                 );
             } else if (e.getCommands().contains("rules")) {
-                p.sendMessage(ChatColor.GOLD + "You are now in " + ChatColor.RED + "Survival");
+                p.sendMessage(ChatColor.WHITE + "\n" +
+                        ChatColor.WHITE + "1. Be respectful of others\n" +
+                        ChatColor.GRAY + "Do not disrespect, impersonate, nor harass other server members. Usage of excessive toxicity and hate speech is prohibited. Do not threaten to Dox, DDoS, Swat or IP grab players.\n" +
+                        ChatColor.GRAY + "\n" +
+                        ChatColor.WHITE + "2. No advertising or self-promotion.\n" +
+                        ChatColor.GRAY + "Do not post any external discord invites, servers, YouTube channels, or Twitch channels.\n" +
+                        ChatColor.GRAY + "\n" +
+                        ChatColor.WHITE + "3. No cheating\n" +
+                        ChatColor.GRAY + "Do not use mods, hacks, or clients that give you an advantage over others. Do not abuse glitches and bugs in the server.\n" +
+                        ChatColor.GRAY + "\n" +
+                        ChatColor.WHITE + "4. Do not disrupt the server\n" +
+                        ChatColor.GRAY + "Do not attempt to lag or crash the server. Do not trade in-game items for real money. No repeated or excessive messages.\n" +
+                        ChatColor.GRAY + "\n" +
+                        ChatColor.WHITE + "5. Do not attempt to bot/boost yourself\n" +
+                        ChatColor.GRAY + "Using bots to gain a higher ranking in the game is not allowed. You have to gain stats legitimately."
+                );
             }
         }
     }
