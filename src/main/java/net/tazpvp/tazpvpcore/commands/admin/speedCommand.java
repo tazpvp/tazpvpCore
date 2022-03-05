21b15b1995bf02dcf19ee41ae06b1f872a78fc83
rownox <rownox.ik@gmail.com>
2022-03-05 14:17:57 -0500

    speed cmd 2

--- src/main/java/net/tazpvp/tazpvpcore/commands/admin/speedCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/admin/speedCommand.java
@@ -1,6 +1,7 @@
 package net.tazpvp.tazpvpcore.commands.admin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,10 +17,12 @@ public class speedCommand implements CommandExecutor {
                 if (args.length < 2) {
                     p.setWalkSpeed(speed);
                     p.setFlySpeed(speed);
+                    p.sendMessage(ChatColor.GOLD + "Speed: " + ChatColor.RED + speed);
                 } else if (args.length == 1) {
                     Player target = Bukkit.getServer().getPlayer(args[0]);
                     target.setWalkSpeed(speed);
                     target.setFlySpeed(speed);
+                    target.sendMessage(ChatColor.GOLD + "Speed: " + ChatColor.RED + speed);
                 }
             }
         }
