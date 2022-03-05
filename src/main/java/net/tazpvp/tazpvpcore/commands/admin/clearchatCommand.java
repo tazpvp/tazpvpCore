f28dd3e840d61e03dbe3f232dc2054042eda0b3a
BubbleGum <taletale155@gmail.com>
2022-03-04 19:42:28 -0500

    tutor rownox

--- src/main/java/net/tazpvp/tazpvpcore/commands/admin/clearchatCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/admin/clearchatCommand.java
@@ -1,6 +1,7 @@
 package net.tazpvp.tazpvpcore.commands.admin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,9 +12,14 @@ public class clearchatCommand implements CommandExecutor {
     public boolean onCommand (CommandSender sender, Command command, String label, String[] args){
         if (sender instanceof Player p){
             if (p != null && p.hasPermission("tazpvp.staff.clearchat")){
-                for (p : Bukkit.getOnlinePlayers()){
-                   if (!p.hasPermission("tazpvp.staff.clearchat")){
+                for (Player player : Bukkit.getOnlinePlayers()){
+                   if (!player.hasPermission("tazpvp.staff.clearchat")){
+                       for (int i = 0; i<100; ++i ){
+                           player.sendMessage(" ");
+                       }
 
+                   } else{
+                       player.sendMessage(ChatColor.GREEN + "Chat full of empty spaces");
                    }
                 }
 
