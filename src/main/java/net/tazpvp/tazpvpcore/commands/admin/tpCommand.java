bd47c4de4bd6382c36fd6c55ed24eca460edb397
BubbleGum <taletale155@gmail.com>
2022-03-04 19:03:03 -0500

    tutor rownox

--- src/main/java/net/tazpvp/tazpvpcore/commands/admin/tpCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/admin/tpCommand.java
@@ -35,14 +35,12 @@ public class tpCommand implements CommandExecutor {
                     int y = Integer.parseInt(args[1]);
                     int z = Integer.parseInt(args[2]);
                     p.teleport(new Location(p.getWorld(), x, y, z));
-                    p.sendMessage(ChatColor.GREEN + "Teleported you to " + x + " " + y + " " + z);
                 } else if (args.length == 4) {
                     World w = Bukkit.getWorld(args[0]);
                     int x = Integer.parseInt(args[1]);
                     int y = Integer.parseInt(args[2]);
                     int z = Integer.parseInt(args[3]);
                     p.teleport(new Location(w, x, y, z));
-                    p.sendMessage(ChatColor.GREEN + "Teleported you to " + w.getName() + " " + x + " " + y + " " + z);
                 } else if (args.length == 5) {
                     Player target = Bukkit.getPlayer(args[0]);
                     World w = Bukkit.getWorld(args[1]);
