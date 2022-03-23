a3e48e6bcbaa7cdc13020ff69b2b5897ac63aa0c
rownox <rownox.ik@gmail.com>
2022-03-23 18:59:19 -0400

    Updated playtime and optimized imp

--- src/main/java/net/tazpvp/tazpvpcore/commands/admin/gmcCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/admin/gmcCommand.java
@@ -1,7 +1,6 @@
 package net.tazpvp.tazpvpcore.commands.admin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
--- src/main/java/net/tazpvp/tazpvpcore/commands/player/playtimeCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/player/playtimeCommand.java
@@ -1,8 +1,6 @@
 package net.tazpvp.tazpvpcore.commands.player;
 
-import net.tazpvp.tazpvpcore.TazpvpCore;
 import net.tazpvp.tazpvpcore.utils.stringUtils;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -15,24 +13,19 @@ import org.bukkit.entity.Player;
 public class playtimeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = null;
-        if(sender instanceof Player) {
-            player = (Player) sender;
-        }
-        if(player != null) {
-            if(args.length == 0) {
-                int seconds = player.getStatistic(Statistic.PLAY_ONE_MINUTE)/20;
-                player.sendMessage(ChatColor.GREEN + "Your playtime is " + ChatColor.WHITE + stringUtils.secondsToDDHHMMSS(seconds));
+        if(sender instanceof Player p) {
+            if (args.length == 0) {
+                int seconds = p.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+                p.sendMessage(ChatColor.GREEN + "Your playtime is " + ChatColor.WHITE + stringUtils.secondsToDDHHMMSS(seconds));
             } else {
                 OfflinePlayer target = Bukkit.getPlayer(args[0]);
-                if(target.isOnline()) {
-                    int seconds = target.getPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE)/20;
-                    player.sendMessage(ChatColor.GREEN + target.getName() + "'s playtime is " + ChatColor.WHITE + stringUtils.secondsToDDHHMMSS(seconds));
+                if (target.isOnline()) {
+                    int seconds = target.getPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+                    p.sendMessage(ChatColor.GREEN + target.getName() + "'s playtime is " + ChatColor.WHITE + stringUtils.secondsToDDHHMMSS(seconds));
                 }
             }
-
-        }  else {
-            player.sendMessage(ChatColor.RED + args[0]+" is not a valid player name.");
+        } else {
+            sender.sendMessage(ChatColor.RED + "Please enter a valid username.");
         }
         return true;
     }
