d3935ddac8b86fcb5c58dbe4f6e4480d68e128b0
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-03-25 17:55:02 -0600

    added arguemtns to ci

--- src/main/java/net/tazpvp/tazpvpcore/commands/admin/CICOMMAND.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/admin/CICOMMAND.java
@@ -1,5 +1,7 @@
 package net.tazpvp.tazpvpcore.commands.admin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,8 +10,16 @@ import org.bukkit.entity.Player;
 public class CICOMMAND implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player p) {
-            p.getInventory().clear();
+        if (args.length > 1) {
+            if (Bukkit.getPlayer(args[0]) != null) {
+                Player target = Bukkit.getPlayer(args[0]);
+                target.getInventory().clear();
+                sender.sendMessage(ChatColor.GOLD + "Cleared " + ChatColor.RED +  target.getName() + ChatColor.GOLD + "'s inventory");
+            }
+        } else {
+            if (sender instanceof Player p) {
+                p.getInventory().clear();
+            }
         }
         return true;
     }
