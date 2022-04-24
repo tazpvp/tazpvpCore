944f07309ca0c85e157dd0ca19d2445723b64200
rownox <rownox.ik@gmail.com>
2022-04-23 23:19:04 -0400

    nick 2

--- src/main/java/net/tazpvp/tazpvpcore/commands/player/nickCommand.java
+++ src/main/java/net/tazpvp/tazpvpcore/commands/player/nickCommand.java
@@ -11,21 +11,29 @@ public class nickCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        Player target = Bukkit.getServer().getPlayer(args[0]);
-        String nick = args[1];
-        if (target != null) {
-            if (nick == null) {
-                target.setDisplayName(target.getName());
-                target.setPlayerListName(target.getName());
-                target.sendMessage(ChatColor.DARK_AQUA + "Your nickname was reset.");
-            } else if (nick.length() > 14) {
-                commandSender.sendMessage(ChatColor.RED + "The nickname is too long");
+        if (commandSender instanceof Player p) {
+            if (p.hasPermission("tazpvp.nick")) {
+                String nick = args[0];
+                nick(p, nick);
             } else {
-                target.setDisplayName(nick);
-                target.setPlayerListName(nick);
-                target.sendMessage(ChatColor.DARK_AQUA + "Your nickname was set to " + ChatColor.AQUA + nick);
+                String nick = args[1];
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                nick(target, nick);
             }
         }
+
         return true;
+    }
+
+    public void nick(Player p, String nick) {
+        if (nick == null) {
+            p.setDisplayName(p.getName());
+            p.setPlayerListName(p.getName());
+            p.sendMessage(ChatColor.DARK_AQUA + "Your nickname was reset.");
+        } else {
+            p.setDisplayName(nick);
+            p.setPlayerListName(nick);
+            p.sendMessage(ChatColor.DARK_AQUA + "Your nickname was set to " + ChatColor.AQUA + nick);
+        }
     }
 }
