e9d91f92c5fbcb0a75ac8f7f3d959074f7c4a532
rownox <rownox.ik@gmail.com>
2022-05-21 22:38:44 -0400

    apply cmd

---
+++ src/main/java/net/tazpvp/tazpvpcore/commands/player/applyCMD.java
@@ -0,0 +1,29 @@
+package net.tazpvp.tazpvpcore.commands.player;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.tazpvp.tazpvpcore.utils.LinkUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class applyCMD implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (commandSender instanceof Player p) {
+            p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            p.sendMessage(ChatColor.DARK_AQUA + " To apply, go to the " + ChatColor.WHITE + "#commands " + ChatColor.DARK_AQUA + "channel in the discord\n" + " and type " + ChatColor.WHITE + "/apply staff");
+            p.spigot().sendMessage(LinkUtils.discordInvite("Click Here to apply."));
+            p.sendMessage(ChatColor.DARK_GRAY + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/main/java/net/tazpvp/tazpvpcore/utils/LinkUtils.java
@@ -0,0 +1,17 @@
+package net.tazpvp.tazpvpcore.utils;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+
+public class LinkUtils {
+    public static TextComponent discordInvite(String name) {
+        TextComponent msg = new TextComponent(ChatColor.BOLD + "" + ChatColor.YELLOW + name);
+        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§fDiscord Server").create()));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/HvuAxcREr2"));
+
+        return msg;
+    }
+}
