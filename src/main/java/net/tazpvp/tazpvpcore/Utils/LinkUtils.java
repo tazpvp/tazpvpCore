02a4ad5481e818f2d4ac726b957f4b6aa6f9ac81
rownox <rownox.ik@gmail.com>
2022-06-09 17:21:16 -0400

    discord link

--- src/main/java/net/tazpvp/tazpvpcore/Utils/LinkUtils.java
+++ src/main/java/net/tazpvp/tazpvpcore/Utils/LinkUtils.java
@@ -10,7 +10,7 @@ public class LinkUtils {
     public static TextComponent discordInvite(String name) {
         TextComponent msg = new TextComponent(ChatColor.BOLD + "" + ChatColor.YELLOW + name);
         msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§fDiscord Server").create()));
-        msg.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/HvuAxcREr2"));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/56rdkbSqa8"));
 
         return msg;
     }
