955176639fb19ae93af3cf556e72fcbbef340dbb
rownox <rownox.ik@gmail.com>
2022-06-10 10:11:51 -0400

    updated help 5

--- src/main/java/net/tazpvp/tazpvpcore/Commands/player/HelpCMD.java
+++ src/main/java/net/tazpvp/tazpvpcore/Commands/player/HelpCMD.java
@@ -36,29 +36,15 @@ public class HelpCMD implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (commandSender instanceof Player p) {
             if (args.length < 1) {
-                help(p, 1);
+                p.sendMessage(s1);
             } else if (args.length == 1 && args[0].equals("2")) {
-                help(p, 2);}
+                p.sendMessage(s2);
+            } else if (args.length == 1 && args[0].equals("3")) {
+                p.sendMessage(s3);
+            } else {
+                p.sendMessage(ChatColor.RED + "Invalid page.");
+            }
         }
         return false;
-    }
-
-    public void help(Player p, int page) {
-        if (page == 1) {
-            p.sendMessage(s1);
-        } else if (page == 2) {
-            p.sendMessage("" +
-                    ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.\n" +
-                    ChatColor.WHITE + "/apply " + ChatColor.GRAY + "Apply for a staff position.\n" +
-                    ChatColor.WHITE + "/spawn " + ChatColor.GRAY + "Teleport back to the spawn.\n" +
-                    ChatColor.WHITE + "/discord " + ChatColor.GRAY + "Join the community.\n" +
-                    ChatColor.WHITE + "/duel " + ChatColor.GRAY + "Battle versus another player.\n" +
-                    ChatColor.WHITE + "/ad " + ChatColor.GRAY + "Receive the server advertisement to copy.\n" +
-                    ChatColor.GRAY + "\n" +
-                    ChatColor.GREEN + "[2/2] " + ChatColor.GRAY + "type '/help' to go back"
-            );
-        } else {
-            p.sendMessage(ChatColor.RED + "Invalid page.");
-        }
     }
 }
