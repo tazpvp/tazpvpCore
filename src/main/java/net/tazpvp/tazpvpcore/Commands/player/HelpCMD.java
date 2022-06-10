6938a8332ebd2dae4003400e2fd1f3f389c0a187
rownox <rownox.ik@gmail.com>
2022-06-10 09:53:44 -0400

    updated help 3

--- src/main/java/net/tazpvp/tazpvpcore/Commands/player/HelpCMD.java
+++ src/main/java/net/tazpvp/tazpvpcore/Commands/player/HelpCMD.java
@@ -8,13 +8,30 @@ import org.bukkit.entity.Player;
 
 public class HelpCMD implements CommandExecutor {
     String s1 = " \n" +
-            ChatColor.GRAY + "" + ChatColor.BOLD + " HOW TO GET:\n" +
-            ChatColor.AQUA + " Shards: " + ChatColor.DARK_AQUA + "Levels, Heads, Generator\n" +
-            ChatColor.YELLOW + " Coins: " + ChatColor.GOLD + "Kills, Achievements, Streaks, Ores\n" +
-            ChatColor.LIGHT_PURPLE + " EXP: " + ChatColor.DARK_PURPLE + "Kills, Achievements,\n" +
-            ChatColor.RED + " Extra HP: " + ChatColor.DARK_RED + "Rebirth, Fat Perk,\n" +
+            ChatColor.GRAY + "" + ChatColor.BOLD + " HOW TO GET (Stats):\n" +
+            ChatColor.AQUA + " Shards: " + ChatColor.GRAY + "Levels, Heads, Generator\n" +
+            ChatColor.YELLOW + " Coins: " + ChatColor.GRAY + "Kills, Achievements, Streaks, Ores\n" +
+            ChatColor.LIGHT_PURPLE + " EXP: " + ChatColor.GRAY + "Kills, Achievements\n" +
+            ChatColor.RED + " Extra HP: " + ChatColor.GRAY + "Rebirth, Perks\n" +
             " \n" +
-            ChatColor.GREEN + " [1/2]" + ChatColor.GRAY + " type '/help 2' for the next page.";
+            ChatColor.GREEN + " [1/2]" + ChatColor.DARK_GREEN + " type '/help 2' for the next page.";
+    String s2 = " \n" +
+            ChatColor.GRAY + "" + ChatColor.BOLD + " HOW TO GET (Items):\n" +
+            ChatColor.RED + " Lorenzo: " + ChatColor.GRAY + "Main Center\n" +
+            ChatColor.YELLOW + " Maxim: " + ChatColor.GRAY + "Item Shop\n" +
+            ChatColor.LIGHT_PURPLE + " Rigel: " + ChatColor.GRAY + "Kills, Achievements\n" +
+            ChatColor.RED + " Extra HP: " + ChatColor.GRAY + "Rebirth, Perks\n" +
+            " \n" +
+            ChatColor.GREEN + " [1/2]" + ChatColor.DARK_GREEN + " type '/help 3' for the next page.";
+    String s3 = " \n" +
+            ChatColor.WHITE + "/report " + ChatColor.GRAY + "Report rule breakers.\n" +
+            ChatColor.WHITE + "/apply " + ChatColor.GRAY + "Apply for a staff position.\n" +
+            ChatColor.WHITE + "/spawn " + ChatColor.GRAY + "Teleport back to the spawn.\n" +
+            ChatColor.WHITE + "/discord " + ChatColor.GRAY + "Join the community.\n" +
+            ChatColor.WHITE + "/duel " + ChatColor.GRAY + "Battle versus another player.\n" +
+            ChatColor.WHITE + "/ad " + ChatColor.GRAY + "Receive the server advertisement to copy.\n" +
+            " \n" +
+            ChatColor.GREEN + " [1/2]" + ChatColor.DARK_GREEN + " type '/help' for the next page.";
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (commandSender instanceof Player p) {
