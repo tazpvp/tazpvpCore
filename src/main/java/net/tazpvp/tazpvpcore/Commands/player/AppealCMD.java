6215b01d20d823e5692756789a21b3e54bb4965b
rownox <rownox.ik@gmail.com>
2022-06-10 16:09:35 -0400

    commands

--- src/main/java/net/tazpvp/tazpvpcore/Commands/player/AppealCMD.java
+++ src/main/java/net/tazpvp/tazpvpcore/Commands/player/AppealCMD.java
@@ -13,7 +13,7 @@ public class AppealCMD implements CommandExecutor {
         if (commandSender instanceof Player p) {
             p.sendMessage(ChatColor.DARK_GRAY + "");
             p.sendMessage(ChatColor.GRAY + " If you wish to be unmuted or unbanned:");
-            p.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.DARK_AQUA + " Contact support: Click below and go to the " + ChatColor.WHITE + "#support " + ChatColor.DARK_AQUA + "channel in our " + ChatColor.AQUA + " [Discord]"));
+            p.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.DARK_AQUA + " Contact support: Click below and go to the " + ChatColor.WHITE + "#support " + ChatColor.DARK_AQUA + "channel in our " + ChatColor.AQUA + "[Discord]"));
             p.sendMessage(ChatColor.DARK_GRAY + "");
         }
         return false;
--- src/main/java/net/tazpvp/tazpvpcore/Commands/player/ApplyCMD.java
+++ src/main/java/net/tazpvp/tazpvpcore/Commands/player/ApplyCMD.java
@@ -12,7 +12,7 @@ public class ApplyCMD implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (commandSender instanceof Player p) {
             p.sendMessage(ChatColor.DARK_GRAY + "");
-            p.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.AQUA + "[Click Here] " + ChatColor.DARK_AQUA + "to join the discord"));
+            p.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.AQUA + " [Click Here] " + ChatColor.DARK_AQUA + "to join the discord"));
             p.sendMessage(ChatColor.DARK_AQUA + " Then go to the " + ChatColor.AQUA + "#commands " + ChatColor.DARK_AQUA + "channel\n" + " type " + ChatColor.AQUA + "/apply (staff)");
             p.sendMessage(ChatColor.DARK_GRAY + "");
         }
