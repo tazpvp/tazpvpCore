3ccc865d810a48100c0bee85494923624c7ae37c
rownox <rownox.ik@gmail.com>
2022-07-14 16:54:47 -0400

    reply

--- src/main/java/net/tazpvp/tazpvpcore/Commands/player/ReplyCMD.java
+++ src/main/java/net/tazpvp/tazpvpcore/Commands/player/ReplyCMD.java
@@ -18,8 +18,8 @@ public class ReplyCMD implements CommandExecutor {
             if (TazpvpCore.messages.containsKey(p.getUniqueId())) {
                 Player target = Bukkit.getPlayer(TazpvpCore.messages.get(p.getUniqueId()));
                 if (target != null) {
-                    target.sendMessage(ChatColor.DARK_AQUA + "From " + ChatColor.AQUA + p.getName() + ": "  + ChatColor.WHITE + msg);
-                    p.sendMessage(ChatColor.DARK_AQUA + "To " + ChatColor.AQUA + target.getName() + ": " + ChatColor.WHITE + msg);
+                    target.sendMessage(ChatColor.DARK_AQUA + "From " + ChatColor.AQUA + p.getName() + ": "  + ChatColor.RED + msg);
+                    p.sendMessage(ChatColor.DARK_AQUA + "To " + ChatColor.AQUA + target.getName() + ": " + ChatColor.RED + msg);
                     TazpvpCore.messages.put(p.getUniqueId(), target.getUniqueId());
                     TazpvpCore.messages.put(target.getUniqueId(), p.getUniqueId());
                     return true;
